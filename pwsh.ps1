9510797141384d6e0dedff0c06c17713f580af4d
Justin <justinsloan@users.noreply.github.com>
2023-01-07 22:38:57 -1000

    Update pwsh.ps1

--- pwsh.ps1
+++ pwsh.ps1
@@ -7,4 +7,16 @@ function Say-HelloWorld {
     Write-Host "Hello, World."
 }
 
+# reset password
+# update user's legal name
+# change user's primary email address
+# add user to group
+# remove user from group
+# list group members
+# list user's group memberships
+# unlock AD account
+# disable AD account
+# add new computer to AD
+# remove computer from AD
+
 Write-Host "PWSH scripts imported sucessfully." -ForegroundColor Green
