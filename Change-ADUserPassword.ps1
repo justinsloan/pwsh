3634be0de51fbf8941262eade04d442b09f27f24
Justin <justinsloan@users.noreply.github.com>
2023-01-09 16:57:40 -1000

    Update Change-ADUserPassword.ps1

--- Change-ADUserPassword.ps1
+++ Change-ADUserPassword.ps1
@@ -2,7 +2,17 @@
 function Change-ADUserPassword {
   $user = Read-Host -Prompt "User"
   $newpass = Read-Host -Prompt "New Password" -MaskInput
+  $requirechange = Read-Host -Prompt "Require password change at next login?"
   Import-Module ActiveDirectory
   Set-ADAccountPassword -Identity $user -Reset -NewPassword (ConvertTo-SecureString -AsPlainText $newpass -Force)
   Unlock-ADAccount -Identity $user
+  
+  if (-not($requirechange)) {
+    $requirechange = $true
+  } elseif ($requirechange -ne "false") {
+    $requirechange = $true
+  }
 }
+
+
+Set-ADUser -Identity $user -ChangePasswordAtLogon $true
